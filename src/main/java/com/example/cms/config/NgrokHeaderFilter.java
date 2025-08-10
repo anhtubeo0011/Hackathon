e88a282dd5614f6fbfecc56e9f3cbcdb5103f783
tungdz202 <anhthu28102000@gmail.com>
2025-08-10 14:57:59 +0700

    commit code

---
+++ src/main/java/com/example/cms/config/NgrokHeaderFilter.java
@@ -0,0 +1,18 @@
+package com.example.cms.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NgrokHeaderFilter implements Filter {
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    if (response instanceof HttpServletResponse) {
+      ((HttpServletResponse) response).setHeader("ngrok-skip-browser-warning", "true");
+    }
+    chain.doFilter(request, response);
+  }
+}
