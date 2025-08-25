8fdb594917010a235751229469ad562f04422201
tungdz202 <anhthu28102000@gmail.com>
2025-08-25 16:52:54 +0700

    commit code

---
+++ src/main/java/com/example/cms/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.cms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Cho phép tất cả API
+            .allowedOrigins("*") // Cho phép tất cả origin (có thể thay * bằng ngrok domain cho an toàn hơn)
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .exposedHeaders("ngrok-skip-browser-warning")
+            .allowCredentials(false);
+      }
+    };
+  }
+}
